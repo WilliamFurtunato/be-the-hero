1cd1d39cc16405e336c080f59841aa2ad00ee128
William Furtunato da Silva <williamfurtunato@hotmail.com>
2020-03-28 15:08:42 -0300

    front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import './global.css';
+
+import Routes from './routes';
+
+function App() {
+  return <Routes />;
 }
 
 export default App;
